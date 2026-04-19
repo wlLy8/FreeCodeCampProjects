9acd609a258d7002c5aa74e92c444319ff35c51e
wlly <william-ly@gmx.de>
2026-04-19 21:12:10 +0200

    Start salon scheduler project

---
+++ RelationalDatabase/Salon Appointment Scheduler/salon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# query the database in your script with:
+# psql --username=freecodecamp --dbname=salon -c "SQL QUERY HERE"
---
+++ RelationalDatabase/Salon Appointment Scheduler/salon.sql
@@ -0,0 +1,11 @@
+--SAVE progress:
+-- pg_dump -cC --inserts -U freecodecamp salon > salon.sql
+
+
+-- RESUME BY RUNNING IN TERMINAL:
+-- psql -U postgres < salon.sql
+
+
+-- LOGIN TO POSTGRESQL:
+-- psql --username=freecodecamp --dbname=postgres
+
