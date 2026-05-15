59f8044d136dde502c5c81534197070eb42848bb
wlly <william-ly@gmx.de>
2026-05-15 16:36:47 +0200

    feature: working more on script by creating functions

--- RelationalDatabase/Periodic Table Database/element.sh
+++ RelationalDatabase/Periodic Table Database/element.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+
+DATABASE_LOOKUP(){
 echo "Please provide an element as an argument."
-
 echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM properties WHERE")
+TYPE=$($PSQL "SELECT type FROM properties WHERE")
+ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE")
+MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE")
+BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE")
+ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE")
+SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE")
+
+
+
+echo "I could not find that element in the database."
+
+}
+
+DATABASE_LOOKUP $1
