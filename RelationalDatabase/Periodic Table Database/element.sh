38f1ffc8fb8a1598bdda9d63a2625cec2277bc54
wlly <william-ly@gmx.de>
2026-05-17 20:50:05 +0200

    feature:creating sh file

--- RelationalDatabase/Periodic Table Database/element.sh
+++ RelationalDatabase/Periodic Table Database/element.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-
+INPUT=$1
 
 DATABASE_LOOKUP(){
-echo "Please provide an element as an argument."
-echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
-ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM properties WHERE")
-TYPE=$($PSQL "SELECT type FROM properties WHERE")
-ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE")
-MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE")
-BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE")
-ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE")
-SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE")
+if [[ -z $INPUT ]]
+then
+    echo "Please provide an element as an argument."
+    exit
+fi
 
+if [[ -n $INPUT ]]
+then
+    echo "Will proceess."
+    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE elements::text ~ '\y${INPUT}\y';")
+    
+    if [[ -z $ATOMIC_NUMBER ]]
+    then
+        echo "I could not find that element in the database."
+        exit
+    fi
 
+    TYPE=$($PSQL "SELECT type FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER;")
+    ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER;")
+    SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER;")
+    echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 
-echo "I could not find that element in the database."
-
+fi
 }
 
-DATABASE_LOOKUP $1
+DATABASE_LOOKUP $INPUT
--- RelationalDatabase/Periodic Table Database/periodic_table.sql
+++ RelationalDatabase/Periodic Table Database/periodic_table.sql
@@ -4,23 +4,35 @@ ALTER TABLE properties RENAME COLUMN melting_point TO melting_point_celsius;
 ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
 
 -- change column constraints
-ALTER TABLE properties ALTER COLUMN melting_point_celsius, boiling_point_celsius SET NOT NULL;
+ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;
+ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;
 ALTER TABLE elements ADD CONSTRAINT uq_elements_symbol UNIQUE (symbol);
 ALTER TABLE elements ADD CONSTRAINT uq_elements_name UNIQUE (name);
-ALTER TABLE elements ALTER COLUMN symbol, name SET NOT NULL;
+ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;
+ALTER TABLE elements ALTER COLUMN name SET NOT NULL;
 ALTER TABLE properties ADD FOREIGN KEY (atomic_number) REFERENCES elements(atomic_number);
 
 
 CREATE TABLE types (type_id INT PRIMARY KEY, type VARCHAR NOT NULL);
+
+-- ZEILEN 18-20 noch anpassen
 INSERT INTO types (type_id, type) VALUES (1, 'alcohol'), (2, 'acid'), (3, 'carbohydrate');
+ALTER TABLE properties ADD COLUMN type_id INT NOT NULL;
 ALTER TABLE properties ADD FOREIGN KEY (type_id) REFERENCES types(type_id);
-ALTER TABLE properties ALTER COLUMN type_id TYPE INT;
-ALTER TABLE properties ALTER COLUMN type_id SET NOT NULL;
+-- ALTER TABLE properties ALTER COLUMN type_id TYPE INT;
+-- ALTER TABLE properties ALTER COLUMN type_id SET NOT NULL;
 
 -- capitalize the first letter of each symbol in the elements table
 UPDATE elements SET symbol = UPPER(LEFT(symbol, 1)) || LOWER(SUBSTRING(symbol, 2));
 ALTER TABLE properties ALTER COLUMN atomic_mass TYPE DECIMAL;
+UPDATE properties SET atomic_mass = atomic_mass::decimal;
 
 -- add values
 INSERT INTO elements (atomic_number, symbol, name) VALUES (9, 'F', 'Fluorine'), (10, 'Ne', 'Neon');
-INSERT INTO properties (atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius) VALUES (9, nonmetal, 18.998, -220, -188.1), (10, 'nonmetal', 20.18, -248.6, -246.1);
+INSERT INTO properties (atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius) VALUES (9, 'nonmetal', 18.998, -220, -188.1), (10, 'nonmetal', 20.18, -248.6, -246.1);
+
+ALTER TABLE properties DROP column type;
+
+-- drop a row
+DELETE FROM elements WHERE atomic_number = 1000;
+DELETE FROM properties WHERE atomic_number = 1000;
